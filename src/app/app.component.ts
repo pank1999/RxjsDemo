885a16325e7f1a4134e7d0a91447a7e32155e556
pank1999 <pankajneeraj541@gmail.com>
2022-10-14 19:09:56 +0530

    initail commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { delay, interval, map, Observable, retry, retryWhen, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor( private http:HttpClient ){}
   title = 'RxjsDemo';
+  users$!:Observable<any>;
+   user!:any;
+
+   source=interval(2000);
+
+  getUser(){
+
+      this.http.get("https://api.github.com/users/pank1999")
+      .pipe( 
+            retry(4)
+         )     
+      .subscribe(
+      res=>{
+        console.log(res);
+        this.user=res;
+      }
+    );
+
+
+    // this.source.pipe(
+    //    map(val=>{
+    //       //console.log(val)
+    //       if(val>5){
+    //         throw val;
+    //       }
+    //       return val
+    //    }),
+    //    retryWhen(error=>
+    //       error.pipe(
+    //         tap(value=>console.log("retryWhen",value)),
+    //         delay(5000)
+    //       )
+    //    )
+
+    // ).subscribe(res=>console.log(res))
+  }
+
+
 }
